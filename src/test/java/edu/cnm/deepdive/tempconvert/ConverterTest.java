bc61cd179ce4e7bb01068828dfa5015bbc27dadd
Kevin Dilts <kdilts@cnm.edu>
2026-01-27 10:35:42 -0700

    parameterized test implemented and passing for valid inputs

--- src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
@@ -4,18 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class ConverterTest {
   static final double TOLERANCE_SCALE = 0.0001;
+  static final double MIN_ABSOLUTE_TOLERANCE = 0.000000001;
 
   @ParameterizedTest
-  void convertC2F(double celsius, double expected) {
+  @CsvFileSource(resources = "convert-c2f-valid.csv", useHeadersInDisplayName = true)
+  void convertC2F_valid(double celsius, double expected) {
     Converter converter = new Converter();
 
+    double tolerance = Math.max(
+        Math.abs(expected * TOLERANCE_SCALE),
+        MIN_ABSOLUTE_TOLERANCE
+    );
+
     assertEquals(
-        -40,
-        converter.convertC2F(-40),
-        Math.abs(-40 * TOLERANCE_SCALE)
+        expected,
+        converter.convertC2F(celsius),
+        tolerance
     );
   }
 }
