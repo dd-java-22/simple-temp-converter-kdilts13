197fee4b937823faed21bfdb6dcb41a1fa8e651a
Kevin Dilts <kdilts@cnm.edu>
2026-02-04 13:37:19 -0700

    update tests to use assertThrows with lambda

--- src/test/java/edu/cnm/deepdive/tempconvert/service/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/tempconvert/service/ConverterTest.java
@@ -1,7 +1,7 @@
 package edu.cnm.deepdive.tempconvert.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -29,14 +29,9 @@ class ConverterTest {
   @ParameterizedTest
   @ValueSource(doubles = {Converter.CELSIUS_ABSOLUTE_ZERO - 0.01, -Double.MAX_VALUE})
   void convertC2F_invalid(double celsius) {
-    try {
+    assertThrows(IllegalArgumentException.class, () -> {
       new Converter().convertC2F(celsius);
-      fail("Illegal argument exception expected, but no exception thrown.");
-    } catch (IllegalArgumentException e) {
-      // Do nothing! This exception is expected.
-    } catch (Exception e) {
-      fail("Illegal argument exception expected, but %s thrown.".formatted(e.getClass().getSimpleName()));
-    }
+    });
   }
 
   @ParameterizedTest
@@ -57,14 +52,9 @@ class ConverterTest {
   @ParameterizedTest
   @ValueSource(doubles = {Converter.FAHRENHEIT_ABSOLUTE_ZERO - 0.01, -Double.MAX_VALUE})
   void convertF2C_invalid(double fahrenheit) {
-    try {
+    assertThrows(IllegalArgumentException.class, () -> {
       new Converter().convertF2C(fahrenheit);
-      fail("Illegal argument exception expected, but no exception thrown.");
-    } catch (IllegalArgumentException e) {
-      // Do nothing! This exception is expected.
-    } catch (Exception e) {
-      fail("Illegal argument exception expected, but %s thrown.".formatted(e.getClass().getSimpleName()));
-    }
+    });
   }
 
 }
